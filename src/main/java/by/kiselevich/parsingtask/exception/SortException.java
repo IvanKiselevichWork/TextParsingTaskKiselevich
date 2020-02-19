bf7d7724b72b27969b4763fbdb6bd209eaf2e3c0
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-19 16:47:30 +0300

    Add SortException

---
+++ src/main/java/by/kiselevich/parsingtask/exception/SortException.java
@@ -0,0 +1,23 @@
+package by.kiselevich.parsingtask.exception;
+
+public class SortException extends Exception {
+
+    public SortException() {
+    }
+
+    public SortException(String message) {
+        super(message);
+    }
+
+    public SortException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SortException(Throwable cause) {
+        super(cause);
+    }
+
+    public SortException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
