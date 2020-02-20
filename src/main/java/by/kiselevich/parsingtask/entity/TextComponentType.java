734804fa4cb56b7f83193937420d7639bbd80ddb
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-20 08:48:59 +0300

    Add regex to TextComponentType

--- src/main/java/by/kiselevich/parsingtask/entity/TextComponentType.java
+++ src/main/java/by/kiselevich/parsingtask/entity/TextComponentType.java
@@ -1,10 +1,21 @@
 package by.kiselevich.parsingtask.entity;
 
 public enum TextComponentType {
-    TEXT,
-    PARAGRAPH,
-    SENTENCE,
-    LEXEME,
-    WORD,
-    SYMBOL
+    TEXT("(?m)(.+)(\n*\t*)"),
+    PARAGRAPH("(?m)([^\\.!?]+)([\\.!?]+)"),
+    SENTENCE("(?m)([^ ]+)( *)"),
+    LEXEME("(?m)([\\w\\d]+)(\\W*)"),
+    WORD("(?m)(\\w+?)(\\W*)"),
+    SYMBOL("");
+
+    private final String regex;
+
+    TextComponentType(String regex) {
+        this.regex = regex;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
 }
