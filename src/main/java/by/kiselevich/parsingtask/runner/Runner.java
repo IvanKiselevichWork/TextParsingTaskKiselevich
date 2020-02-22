91757e65f661890c9e3b0a6abfe99e072a5b3b9f
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-22 14:40:58 +0300

    Add source file reader to runner, add input file, fix TEXT regex to find carriage return

--- src/main/java/by/kiselevich/parsingtask/runner/Runner.java
+++ src/main/java/by/kiselevich/parsingtask/runner/Runner.java
@@ -3,40 +3,48 @@ package by.kiselevich.parsingtask.runner;
 import by.kiselevich.parsingtask.entity.TextComponent;
 import by.kiselevich.parsingtask.entity.TextComponentType;
 import by.kiselevich.parsingtask.exception.SortException;
+import by.kiselevich.parsingtask.exception.SourceTextReaderException;
 import by.kiselevich.parsingtask.exception.TextParseException;
 import by.kiselevich.parsingtask.parser.*;
+import by.kiselevich.parsingtask.reader.SourceTextReader;
 import by.kiselevich.parsingtask.sorter.ParagraphsBySentenceCountSorter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+
 public class Runner {
 
+    private static final String SOURCE_TEXT_FILENAME = "input.txt";
     private static final Logger LOG = LogManager.getLogger(Runner.class);
 
     public static void main(String[] args) {
-        String sourceText = "\tIt has survived - not only (five) centuries, but also the leap into 13<<2 electronic type setting, remaining 3>>5 essentially ~6&9|(3&4) unchanged. It was popularised in the 5|(1&2&(3|(4&(6^5|6&47)|3)|2)|1) with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
-                "\tIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using (~71&(2&3|(3|(2&1>>2|2)&2)|10&2))|78 Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using (Content here), content here', making it look like readable English.\n" +
-                "\tIt is a (4^5|1&2<<(2|5>>2&71))|1200 established fact that a reader will be of a page when looking at its layout.\n" +
-                "\tBye.\n";
-
-        AbstractParser expressionParser = new ExpressionToValueParser();
-        AbstractParser textParser = new TextComponentParser(TextComponentType.TEXT);
-        AbstractParser paragraphParser = new TextComponentParser(TextComponentType.PARAGRAPH);
-        AbstractParser sentenceParser = new TextComponentParser(TextComponentType.SENTENCE);
-        AbstractParser lexemeParser = new TextComponentParser(TextComponentType.LEXEME);
-        AbstractParser wordParser = new TextComponentParser(TextComponentType.WORD);
-        AbstractParser symbolParser = new SymbolToTextLeafParser();
-
-        expressionParser.setNextParser(textParser);
-        textParser.setNextParser(paragraphParser);
-        paragraphParser.setNextParser(sentenceParser);
-        sentenceParser.setNextParser(lexemeParser);
-        lexemeParser.setNextParser(wordParser);
-        wordParser.setNextParser(symbolParser);
 
         try {
-            TextComponent textComponent = expressionParser.parse(sourceText);
+            File sourceTextFile = getFileFromResources(SOURCE_TEXT_FILENAME);
 
+            SourceTextReader sourceTextReader = new SourceTextReader(sourceTextFile);
+
+            String sourceText = sourceTextReader.getSourceText();
+
+            AbstractParser expressionParser = new ExpressionToValueParser();
+            AbstractParser textParser = new TextComponentParser(TextComponentType.TEXT);
+            AbstractParser paragraphParser = new TextComponentParser(TextComponentType.PARAGRAPH);
+            AbstractParser sentenceParser = new TextComponentParser(TextComponentType.SENTENCE);
+            AbstractParser lexemeParser = new TextComponentParser(TextComponentType.LEXEME);
+            AbstractParser wordParser = new TextComponentParser(TextComponentType.WORD);
+            AbstractParser symbolParser = new SymbolToTextLeafParser();
+
+            expressionParser.setNextParser(textParser);
+            textParser.setNextParser(paragraphParser);
+            paragraphParser.setNextParser(sentenceParser);
+            sentenceParser.setNextParser(lexemeParser);
+            lexemeParser.setNextParser(wordParser);
+            wordParser.setNextParser(symbolParser);
+
+            TextComponent textComponent = expressionParser.parse(sourceText);
             LOG.trace("---------Source:");
             LOG.trace("\n{}", sourceText);
             LOG.trace("---------After replacing and parsing");
@@ -46,8 +54,16 @@ public class Runner {
             LOG.trace("---------After sorting paragraphs by sentence count");
             LOG.trace("\n{}", textComponent);
 
-        } catch (TextParseException | SortException e) {
+        } catch (TextParseException | SortException | SourceTextReaderException | FileNotFoundException e) {
             LOG.error(e);
         }
+    }
+
+    private static File getFileFromResources(String filename) throws FileNotFoundException {
+        URL sourceTextUrl = Runner.class.getClassLoader().getResource(filename);
+        if (sourceTextUrl == null) {
+            throw new FileNotFoundException();
+        }
+        return new File(sourceTextUrl.getFile());
     }
 }
